9dc66ecd18c7c78e77354d69ad0f9aa5c76a3042
MNEneas <eneasdeazevedojunior@gmail.com>
2025-04-03 12:06:48 -0400

    Create Cavalo.c

---
+++ Cavalo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+   for (int i = 0; i < 1; i++)
+   {
+    do
+    {
+        printf("Baixo ");
+        i++;
+    }while(i<2);
+
+    printf("Esquerda ");
+   }
+   
+   printf("Seu cavalo se movimentou para a casa de baixo à esquerda\n");
+    return 0;
+}
